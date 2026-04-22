790302f6cc31765cd7669d40a6236ee16a23d2cc
Monisha21-mo <monisha.k.s212003@gmail.com>
2026-04-22 23:34:45 +0530

    Add retriever function using Chroma and embeddings

--- src/retriever.py
+++ src/retriever.py
@@ -1,1 +1,13 @@
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
+def create_retriever(chunks):
+    embedding = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+
+    vectorstore = Chroma.from_documents(
+        documents=chunks,
+        embedding=embedding
+    )
+
+    retriever = vectorstore.as_retriever()
+    return retriever
