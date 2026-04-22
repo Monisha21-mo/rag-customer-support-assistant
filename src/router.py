792d41f15dba09b7e4d86677786485a27ef22681
Monisha21-mo <monisha.k.s212003@gmail.com>
2026-04-22 23:35:59 +0530

    Add route_query function for query categorization

--- src/router.py
+++ src/router.py
@@ -1,1 +1,9 @@
+def route_query(query):
+    query = query.lower()
 
+    if "price" in query or "cost" in query:
+        return "pricing"
+    elif "help" in query or "support" in query:
+        return "support"
+    else:
+        return "general"
