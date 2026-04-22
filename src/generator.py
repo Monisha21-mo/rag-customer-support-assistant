37e23da8cbb45e93fb144aefe742588cc10be5d5
Monisha21-mo <monisha.k.s212003@gmail.com>
2026-04-22 23:35:26 +0530

    answering function in generator.py
    Added a function for question-answering using a retriever and Hugging Face's transformers pipeline.

--- src/generator.py
+++ src/generator.py
@@ -1,1 +1,15 @@
+from transformers import pipeline
 
+qa_pipeline = pipeline("question-answering")
+
+def rag_answer(query, retriever):
+    docs = retriever.invoke(query)
+
+    context = "\n".join([doc.page_content for doc in docs[:2]])
+
+    result = qa_pipeline(
+        question=query,
+        context=context
+    )
+
+    return result['answer']
